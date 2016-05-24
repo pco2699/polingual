30da3858b3df0d0eebfcfbddbc64d5181fb82b3d
pco2699 <aenkun@gmail.com>
2016-05-24 22:26:22 +0900

    Takayama Commit!!!

--- client/app/mypage/calendar/calendar.controller.ts
+++ client/app/mypage/calendar/calendar.controller.ts
@@ -10,7 +10,7 @@ class CalendarComponent {
 angular.module('testAppApp')
   .component('calendar', {
     templateUrl: 'app/mypage/calender/calendar.html',
-    controller: CalenderComponent
+    controller: CalendarComponent
   });
 
 })();
