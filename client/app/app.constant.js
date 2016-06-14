51c905c459fc79cc3794b521a4ba045ca33b5cb9
pco2699 <pco2699@ks-macbook-pro.local>
2016-06-14 22:16:03 +0900

    Takayama

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -3,11 +3,7 @@
 
 angular.module('polingualApp.constants', [])
 
-<<<<<<< HEAD
 .constant('appConfig', {userRoles:['guest','user','admin'],gender:['Male','Female','Other'],langs:['English','Japanese','Spanish']})
-=======
-.constant('appConfig', {userRoles:['guest','user','admin'],gender:['Male','Female','Other']})
->>>>>>> 6882e9148194589afe673be1c66310979b0694f8
 
 ;
 })(angular);
