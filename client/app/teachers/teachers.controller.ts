59c94bba5510b6b5b56769ecf797d8210061aaa1
EC2 Default User <ec2-user@ip-172-31-25-213.ap-northeast-1.compute.internal>
2016-06-29 15:35:03 +0000

    shintani

--- client/app/teachers/teachers.controller.ts
+++ client/app/teachers/teachers.controller.ts
@@ -20,33 +20,22 @@ class TeachersComponent {
     {name: 'Merry', gender: 'Female', country: 'Japan', language: 'Japanese'},
     {name: 'takayama', gender: 'Other', country: 'Japan', language: 'Japanese'}];*/
 
-  public teachers;
+  public teachers = [];
   public tlang;
 
   constructor(public $scope, public $stateParams, public $http) {
-	  /*$scope.$parent.lang = $stateParams.lang;
-	  tlang = $stateParams.lang;
-	  teachers.$promise.then(function(teachers){
-	      this.teachers = teachers;
-	  });
-	  var result = $http.get('/api/users/bylang/' + tlang,{isArray:true});
-          console.log(tlang);
-	  console.log(result);
-	  teachers = result.data;
-	  console.log(teachers);*/
   }
 
   $onInit() {
 	   this.$scope.$parent.lang = this.$stateParams.lang;
 	   this.tlang = this.$stateParams.lang;
-           console.log(this.tlang);
 	   var result = this.$http.get('/api/users/bylang/' + this.tlang,{isArray:true});
-           console.log(this.tlang);
-           console.log(result);
-	   result.then(function(result){
-              this.teachers = result;
+	   this.teachers = result.then(function(result){
+              var teachers = result.data;
+	      console.log(teachers);
+              return teachers;
 	   });
-           console.log(this.teachers);
+	   console.log(this.teachers);
   }
 }
 
--- client/app/teachers/teachers.ts
+++ client/app/teachers/teachers.ts
@@ -4,12 +4,11 @@ angular.module('polingualApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('teachers', {
-        url: '/teachers',
+        url: '/teachers/:lang',
 	resolve: {
-                  teachers: function($stateParams, public $http){
+                  teacherstest: function($stateParams, public $http){
 		  var tlang = $stateParams.lang;
 		  var result = $http.get('/api/users/bylang/' + tlang,{isArray:true});
-		  console.log(tlang);
 		  return result;
 		}
 	},
